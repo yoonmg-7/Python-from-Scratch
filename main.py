d1c4c4384a7ee6924962402dac005aacc11af875
Yoon Nandar Maung <yoonnandarmg024@gmail.com>
2025-08-24 22:03:45 +0630

    [UPDATE] python: Lesson-7 to update

--- main.py
+++
@@ -1,7 +0,0 @@
-# name =input("Name :");
-# print(name)
-
-# pi = 3.14
-# radius = int(input(" Radius :"));
-# area = pi * radius**2
-# print('The area is ' + str(area));
---
+++ opera_condition.py
@@ -0,0 +1,27 @@
+# comparison operator  ( > < >= <= == )
+# logical operator  ( and or not )
+
+# ==================================================
+
+# if else condition
+age = int(input("Enter your age :"))
+
+if age < 18:
+    print( " You are young ");
+
+elif age >= 18 and age < 30:
+    print("You are middle age");
+
+else :
+    print("You are old");
+
+# ================================================
+tired= input("are you tired 'y/n' ; ");
+
+if tired == 'y':
+    print("Rest Well!");
+elif tired=='n':
+    print("Go to back work");
+else:
+    print("Please enter yes/no - y/n");
+
